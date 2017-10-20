45b21592ab2cbb7cf9809be70771c8daf45c6e34
Swarna Muralidharan <swarna.muralic@gmail.com>
2017-10-20 23:13:37 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  !position_taken? && number_entered.between(0, 8)?
+  !position_taken? && index.between(0, 8)?
   true : false
 end
 
